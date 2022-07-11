2e4cfa4553ed2e9bffbdec1f8dc15c42abaa3715
Oscar de la Hera Gomez <oscar@delasign.com>
2022-07-11 12:07:40 -0400

    Initial Commit

---
+++ Starter Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Starter Project
+//
+//  Created by Oscar de la Hera Gomez on 7/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
