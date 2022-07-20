81296299093a63d729f231c5422d72258f65b6f6
Oscar de la Hera Gomez <oscar@delasign.com>
2022-07-20 15:35:05 -0400

    How to add and structure local images in Swift

---
+++ Starter Project/Models/Constants/UIImages.swift
@@ -0,0 +1,13 @@
+//
+//  UIImages.swift
+//  Starter Project
+//
+//  Created by Oscar de la Hera Gomez on 7/20/22.
+//
+
+import Foundation
+import UIKit
+
+struct SampleImages {
+    static let placeholder: String = "SudoImage"
+}
