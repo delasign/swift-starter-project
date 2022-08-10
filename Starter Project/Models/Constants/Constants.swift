6bb3ca250544933a5c8630cff5d421d7ac77c8ed
Oscar de la Hera Gomez <oscar@delasign.com>
2022-08-10 13:51:06 -0400

    Added Constants to starter project

---
+++ Starter Project/Models/Constants/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Starter Project
+//
+//  Created by Oscar de la Hera Gomez on 8/10/22.
+//
+
+import Foundation
+import UIKit
+
+let kPadding: CGFloat = 20
+let kButtonDimension: CGFloat = 60
