2c0cb4c79fd2b9bb03e27aa12b2787c0b87d8b14
Oscar de la Hera Gomez <oscar@delasign.com>
2022-08-16 11:26:13 -0400

    Setup Carousel

--- Starter Project/UI/Screens/CustomUIView/CustomUIView+UI.swift
+++ Starter Project/UI/Screens/CustomUIView/CustomUIView+UI.swift
@@ -13,9 +13,25 @@ extension CustomUIView {
     // The setupUI function should be the only publically available class in this extension.
     // This can be called refreshUI if your app removes and adds content periodically.
     func setupUI() {
-
+        setupCarousel()
     }
 
     // MARK: UI Setup Functionality
-
+    private func setupCarousel() {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            // Add the View
+            self.addSubview(self.carousel)
+            // Setup the constraints
+            self.carousel.top(to: self, offset: 100)
+            self.carousel.left(to: self)
+            self.carousel.right(to: self)
+            self.carousel.height(300)
+            // Register the cell
+            self.carousel.register(CarouselCell.self, forCellWithReuseIdentifier: CarouselCell.identifier)
+            // Set the delegate & datasource
+            self.carousel.delegate = self
+            self.carousel.dataSource = self
+        }
+    }
 }
