744585961d2dedb358177007e68b8477eb704f32
Oscar de la Hera Gomez <oscar@delasign.com>
2022-08-19 19:28:46 +0200

    Create the header style

---
+++ Starter Project/Coordinators/Styleguide/Styleguide+Header.swift
@@ -0,0 +1,39 @@
+//
+//  Styleguide+Header.swift
+//  Starter Project
+//
+//  Created by Oscar de la Hera Gomez on 8/19/22.
+//
+
+import Foundation
+import UIKit
+
+extension Styleguide {
+    static func createHeaderStyle(
+        text: String? = nil
+    ) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        if let text = text {
+            label.attributedText = self.attributedHeaderText(text: text)
+        }
+        label.textAlignment = .left
+        label.sizeToFit()
+        return label
+    }
+
+    static func attributedHeaderText(text: String) -> NSMutableAttributedString {
+        // Paragraph Style
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        paragraphStyle.lineSpacing = 1
+        // Attributed Text
+        let attributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.foregroundColor: Styleguide.colors.black,
+            NSAttributedString.Key.font: UIFont(name: fonts.HelveticaNeueBold, size: 24) ?? "",
+            NSAttributedString.Key.paragraphStyle: paragraphStyle
+        ]
+        return NSMutableAttributedString(string: text, attributes: attributes)
+    }
+}
