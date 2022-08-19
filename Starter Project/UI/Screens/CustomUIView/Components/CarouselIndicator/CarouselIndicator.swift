df9913fa541afdd9dbe20ad3917c3ff3f0b1d46d
Oscar de la Hera Gomez <oscar@delasign.com>
2022-08-19 10:02:38 +0200

    Create the Scroll Indicator

---
+++ Starter Project/UI/Screens/CustomUIView/Components/CarouselIndicator/CarouselIndicator.swift
@@ -0,0 +1,42 @@
+//
+//  CarouselIndicator.swift
+//  Starter Project
+//
+//  Created by Oscar de la Hera Gomez on 8/19/22.
+//
+
+import Foundation
+import UIKit
+
+class CarouselIndicator: UIView {
+    // MARK: Variables
+    static let identifier: String = "[CarouselIndicator]"
+    // MARK: UI
+    // MARK: Lifecycle
+    
+    // This is the function that gets called when you initialize your view.
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // MARK: UI Specific Setup
+        // Before calling your setup functions, call any visual functionality that may be required:
+        // i.e. background color, isHidden, isUserInteractionEnabled or translatesAutoresizingMaskIntoConstraints.
+        self.backgroundColor = .black
+        // MARK: Functionality Setup
+        
+    }
+    
+    // This is the function that gets called when you remove your view from its superview.
+    override func removeFromSuperview() {
+        super.removeFromSuperview()
+    }
+    
+    // viewWillTransition should be called when the view resizes or changes orientation.
+    func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+
+    }
+
+    // This function is required for youa custom UIView.
+    required init?(coder: NSCoder) {
+        fatalError("did not instanstiate coder")
+    }
+}
