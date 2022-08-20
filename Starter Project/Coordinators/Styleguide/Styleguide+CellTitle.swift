59db0851573aa9c316c93aca22cc6c5a6a75fbdb
Oscar de la Hera Gomez <oscar@delasign.com>
2022-08-20 08:35:09 +0200

    Create the cell title style

---
+++ Starter Project/Coordinators/Styleguide/Styleguide+CellTitle.swift
@@ -0,0 +1,39 @@
+//
+//  Styleguide+CellTitle.swift
+//  Starter Project
+//
+//  Created by Oscar de la Hera Gomez on 8/20/22.
+//
+
+import Foundation
+import UIKit
+
+extension Styleguide {
+    static func createCellTitleStyle(
+        text: String? = nil
+    ) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        if let text = text {
+            label.attributedText = self.attributedCellTitleText(text: text)
+        }
+        label.textAlignment = .left
+        label.sizeToFit()
+        return label
+    }
+
+    static func attributedCellTitleText(text: String) -> NSMutableAttributedString {
+        // Paragraph Style
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        paragraphStyle.lineSpacing = 1
+        // Attributed Text
+        let attributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.foregroundColor: Styleguide.colors.black,
+            NSAttributedString.Key.font: UIFont(name: fonts.HelveticaNeueBold, size: 18) ?? "",
+            NSAttributedString.Key.paragraphStyle: paragraphStyle
+        ]
+        return NSMutableAttributedString(string: text, attributes: attributes)
+    }
+}
