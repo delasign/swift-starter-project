e93310d48e8b867793c9ac7d795afa2b2d3b9ce7
Oscar de la Hera Gomez <oscar@delasign.com>
2022-08-20 10:07:59 +0200

    Setup your UICollectionView

--- Starter Project/UI/Screens/CustomUIView/CustomUIView+UI.swift
+++ Starter Project/UI/Screens/CustomUIView/CustomUIView+UI.swift
@@ -13,10 +13,28 @@ extension CustomUIView {
     // The setupUI function should be the only publically available class in this extension.
     // This can be called refreshUI if your app removes and adds content periodically.
     func setupUI() {
-        
+        setupCollectionView()
     }
 
     // MARK: UI Setup Functionality
-    
+    private func setupCollectionView() {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            // Add the UICollectionView to the View
+            self.addSubview(self.collectionView)
+            // Setup the constraints
+            self.collectionView.top(to: self, offset: kPadding)
+            self.collectionView.left(to: self, offset: kPadding)
+            self.collectionView.right(to: self, offset: kPadding)
+            self.collectionView.bottom(to: self)
+            // Register the cells
+            self.collectionView.register(TitleCell.self, forCellWithReuseIdentifier: TitleCell.identifier)
+            self.collectionView.register(SectionACell.self, forCellWithReuseIdentifier: SectionACell.identifier)
+            self.collectionView.register(SectionBCell.self, forCellWithReuseIdentifier: SectionBCell.identifier)
+            // Set the delegate & datasource
+            self.collectionView.delegate = self
+            self.collectionView.dataSource = self
+        }
+    }
 
 }
