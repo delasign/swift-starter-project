c6c9886c019d06d131347a1bfdb6e0e93ac59b09
Oscar de la Hera Gomez <oscar@delasign.com>
2022-08-21 15:24:33 +0200

    Create the HTTP Methods

---
+++ Starter Project/Models/API/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  Starter Project
+//
+//  Created by Oscar de la Hera Gomez on 8/21/22.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case POST
+    case GET
+    case DELETE
+    case PUT
+    case HEAD
+}
