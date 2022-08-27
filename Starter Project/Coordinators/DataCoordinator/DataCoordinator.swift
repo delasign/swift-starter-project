391766290a06cd674ad46dbd2213ba71f6616319
Oscar de la Hera Gomez <oscar@delasign.com>
2022-08-27 20:25:50 +0200

    Update Data Coordinator

--- Starter Project/Coordinators/DataCoordinator/DataCoordinator.swift
+++ Starter Project/Coordinators/DataCoordinator/DataCoordinator.swift
@@ -14,11 +14,19 @@ class DataCoordinator: NSObject {
     static let identifier: String = "[DataCoordinator]"
     static let shared: DataCoordinator = DataCoordinator()
     let configuration: Configuration = Configuration()
+    var nasdaqDataLinkClient: APIClient?
 
     // MARK: Lifecycle
     func initialize() {
-        debugPrint("\(DataCoordinator.identifier) \(DebuggingIdentifiers.actionOrEventInProgress) Initializing... \(DebuggingIdentifiers.actionOrEventInProgress)")
+        debugPrint("\(DataCoordinator.identifier) initialize \(DebuggingIdentifiers.actionOrEventInProgress) Initializing... \(DebuggingIdentifiers.actionOrEventInProgress)")
         // Intialize configuration
         self.configuration.initialize()
+        if let apiKey = self.configuration.getAPIKey(), let baseURL = self.configuration.getBaseURL() {
+            let apiConfiguration: APIConfiguration = APIConfiguration(apiKey: apiKey, url: URL(string: baseURL))
+            self.nasdaqDataLinkClient = APIClient(configuration: apiConfiguration)
+            debugPrint("\(DataCoordinator.identifier) initialize \(DebuggingIdentifiers.actionOrEventSucceded) Initialized Nasdaq Data Link API Client with configuration : \(apiConfiguration).")
+        } else {
+            debugPrint("\(DataCoordinator.identifier) \(DebuggingIdentifiers.actionOrEventFailed) Failed to initialize Nasdaq Data Link API Client.")
+        }
     }
 }
