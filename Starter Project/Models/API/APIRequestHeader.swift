063059a25cbbc820099f0e5d76f574fb67a8418e
Oscar de la Hera Gomez <oscar@delasign.com>
2022-08-27 20:10:50 +0200

    API Client Update + Request Headers

---
+++ Starter Project/Models/API/APIRequestHeader.swift
@@ -0,0 +1,13 @@
+//
+//  APIRequestHeader.swift
+//  Starter Project
+//
+//  Created by Oscar de la Hera Gomez on 8/27/22.
+//
+
+import Foundation
+
+struct APIRequestHeader {
+    let value: String
+    let httpHeaderField: String
+}
