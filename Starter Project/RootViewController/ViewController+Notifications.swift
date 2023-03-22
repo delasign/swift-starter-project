46fcce42b657cfc42c6b92619c87906ca03bf5f7
Oscar de la Hera Gomez <oscar@delasign.com>
2023-03-22 10:26:45 -0400

    Update StoreKit Status everytime app goes to the foreground

--- Starter Project/RootViewController/ViewController+Notifications.swift
+++ Starter Project/RootViewController/ViewController+Notifications.swift
@@ -11,9 +11,17 @@ import UIKit
 extension ViewController {
     // The setupNotifications function should be the only publically available class in this extension.
     func setupNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.onReturnToForeGround), name: UIApplication.willEnterForegroundNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.updateExperienceState), name: SystemNotifications.updateExperienceState, object: nil)
     }
     // MARK: Notification Setup Functionality
+    @objc private func onReturnToForeGround() {
+        // Update StoreKit statuses everytime the app returns to the foreground
+        Task {
+            StoreKitCoordinator.shared.updateCustomerProductStatus()
+        }
+    }
+    
     @objc private func updateExperienceState(notification: Notification) {
         guard let userInfo = notification.userInfo, let state = userInfo[kExperienceStateUserInfo] as? ExperienceStates else {
             debugPrint("\(Landing.identifier) updateExperienceState \(DebuggingIdentifiers.actionOrEventFailed) Update Experience State has no state.")
