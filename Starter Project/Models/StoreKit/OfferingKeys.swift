d4f96d7d32b8f8ddf7efe72b1df2e0177f7197af
Oscar de la Hera Gomez <oscar@delasign.com>
2023-03-31 12:47:00 -0400

    Fix Offering Keys

--- Starter Project/Models/StoreKit/OfferingKeys.swift
+++ Starter Project/Models/StoreKit/OfferingKeys.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 enum OfferingKeys: String {
-    case sampleNonConsumable = "com.delasign.samplenonconsumable"
-    case sampleConsumable = "com.delasign.sampleconsumable"
+    case sampleNonConsumable = "Sample Non-Consumable"
+    case sampleConsumable = "Sample Consumable"
     case sampleNonRenewingSubscription = "Sample Non-Renewing Subscription"
     case sampleAutoRenewableSubscription = "Sample Auto Renewable Subscription"
     case sampleTierTwoAutoRenewableSubscription = "Sample Tier 2 Auto Renewable Subscription"
