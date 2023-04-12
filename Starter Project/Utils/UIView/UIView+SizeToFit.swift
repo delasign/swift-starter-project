1f349589e73c9fc421e69d4ac29c61a2b5f45c45
Oscar de la Hera Gomez <oscar@delasign.com>
2023-04-12 13:49:46 -0400

    Added Size to Fit Utility + Renamed RemoveAllViews to be an extension

---
+++ Starter Project/Utils/UIView/UIView+SizeToFit.swift
@@ -0,0 +1,31 @@
+//
+//  UIView+SizeToFit.swift
+//  Starter Project
+//
+//  Created by Oscar de la Hera Gomez on 4/12/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func getHeightOfContent() -> CGFloat {
+        var height: CGFloat = 0
+        for view in self.subviews {
+            if view.frame.maxY > height {
+                height = view.frame.maxY
+            }
+        }
+        return height
+    }
+    
+    func getWidthOfContent() -> CGFloat {
+        var width: CGFloat = 0
+        for view in self.subviews {
+            if view.frame.maxX > width {
+                width = view.frame.maxX
+            }
+        }
+        return width
+    }
+}
