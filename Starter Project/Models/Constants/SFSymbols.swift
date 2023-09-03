9f6844af7335198f3fcd3e1ead299618194a0d66
Oscar de la Hera Gomez <oscar@delasign.com>
2023-09-03 12:50:20 -0400

    Update Readme Update Custom View Setup UI Update UIImages -> System Images Add SF Symbols Struct

---
+++ Starter Project/Models/Constants/SFSymbols.swift
@@ -0,0 +1,15 @@
+//
+//  SFSymbols.swift
+//  Starter Project
+//
+//  Created by Oscar de la Hera Gomez on 9/3/23.
+//
+
+import Foundation
+
+struct SFSymbols {
+    static let menu: String = "line.3.horizontal"
+    static let close: String = "xmark"
+    static let refresh: String = "arrow.clockwise"
+    static let back: String = "arrow.left"
+}
