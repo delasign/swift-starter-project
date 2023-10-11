922009d8f0bddf87e0855087799b30b8974a7840
Oscar de la Hera Gomez <oscar@delasign.com>
2023-10-11 13:17:33 -0400

    Linting

--- Starter Project/UI/Screens/MetalUIView/MetalUIView+Animations.swift
+++ Starter Project/UI/Screens/MetalUIView/MetalUIView+Animations.swift
@@ -17,16 +17,16 @@ extension MetalUIView {
         // Determine if the frame rate exists and if it is not 0
         // When this value is 0, the preferred frame rate is equal to the maximum refresh rate of the display, as indicated by the maximumFramesPerSecond property.
         if let frameRate = displayLink.preferredFrameRateRange.preferred, frameRate != 0.0 {
-            displayLinkRefreshRate = CGFloat(1/frameRate)
+            displayLinkRefreshRate = CGFloat(1 / frameRate)
             debugPrint("\(MetalUIView.identifier) setupCADisplayLink \(DebuggingIdentifiers.actionOrEventSucceded) Refresh rate is \(frameRate) frames per second. This is the displayLink preferred FPS.")
         } else {
             let frameRate = UIScreen.main.maximumFramesPerSecond
-            displayLinkRefreshRate = CGFloat(1/frameRate)
+            displayLinkRefreshRate = CGFloat(1 / frameRate)
             debugPrint("\(MetalUIView.identifier) setupCADisplayLink \(DebuggingIdentifiers.actionOrEventSucceded) Refresh rate is \(frameRate) frames per second. This matches the screen")
         }
         debugPrint("\(MetalUIView.identifier) setupCADisplayLink \(DebuggingIdentifiers.actionOrEventSucceded) Setup Complete.")
     }
-    
+
     @objc private func updateAnimationVariables() {
         // Update the magnitude and direction
         if magnitudeIsGrowing {
@@ -34,7 +34,7 @@ extension MetalUIView {
         } else {
             magnitude -= displayLinkRefreshRate
         }
-        
+
         if magnitude >= 1 {
             magnitude = 1
             magnitudeIsGrowing = false
@@ -42,7 +42,7 @@ extension MetalUIView {
             magnitude = 0
             magnitudeIsGrowing = true
         }
-        
+
         // determine the new origin
         let screenBounds = getCurrentScreenBounds()
         let initialOrigin: CGPoint = CGPoint(x: screenBounds.midX, y: screenBounds.midY)
