cee8e58abdd0e3e5e96a6471009f4d12e5cdbbeb
Oscar de la Hera Gomez <oscar@delasign.com>
2024-08-05 11:41:52 -0400

    Model on touch

--- Starter Project/UI/Screens/ARCameraView/ARCameraView+Touches.swift
+++ Starter Project/UI/Screens/ARCameraView/ARCameraView+Touches.swift
@@ -15,6 +15,33 @@ extension ARCameraView {
     }
     
     func addModel() {
-        debugPrint("Add Model")
+        // Create anchor using the camera's current position
+        if let currentFrame = sceneView.session.currentFrame {
+           
+          // Create a transform with a translation of 0.2 meters in front of the camera
+          var translation = matrix_identity_float4x4
+          translation.columns.3.z = -0.2
+          let transform = simd_mul(currentFrame.camera.transform, translation)
+           
+          // Add a new anchor to the session
+          let anchor = ARAnchor(transform: transform)
+           
+          guard let modelScene = SCNScene(named: "Capoeira.dae") else { return }
+
+          // Create a node for the model
+          let node = SCNNode()
+          
+            for child in modelScene.rootNode.childNodes {
+                let scale: Float = 0.0001
+                node.scale = SCNVector3(x: scale, y: scale, z: scale)
+                node.addChildNode(child)
+            }
+
+          // Position the model in front of the camera
+          node.position = SCNVector3(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+
+          // Add the node to the scene
+          sceneView.scene.rootNode.addChildNode(node)
+        }
     }
 }
